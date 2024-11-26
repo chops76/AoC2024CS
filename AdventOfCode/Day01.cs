cc1da76ec9ee49e57a86ec0704036a1b50ec80e1
Dan Chopskie <dchopskie@gmail.com>
2024-11-25 23:18:55 -0600

    Initial Checkin

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -1,15 +1,38 @@
-﻿namespace AdventOfCode;
+﻿using System.Globalization;
+
+namespace AdventOfCode;
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
+    private readonly List<List<int>> _input;
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _input = ParseInput();
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1() => new($"{Part_1()}");
+    public override ValueTask<string> Solve_2() => new($"{Part_2()}");
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    private List<List<int>> ParseInput()
+    {
+        var allLines = File.ReadAllText(InputFilePath);
+        var result = allLines
+                         .Split(new[] { "\r\n\r\n", "\n\n" }, StringSplitOptions.None)
+                         .Select(block => block.Split(['\r', '\n'], StringSplitOptions.RemoveEmptyEntries)
+                                               .Select(int.Parse)
+                                               .ToList())
+                         .ToList();
+        return result;
+    }
+
+    public int Part_1() {
+        return _input.Max(sublist => sublist.Sum());
+    }
+
+    public int Part_2() {
+        return _input.OrderByDescending(sublist => sublist.Sum())
+               .Take(3)
+               .Sum(sublist => sublist.Sum());
+    }
 }
