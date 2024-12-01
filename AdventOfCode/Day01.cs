ea4d8f58479e8f9f8ea2074374723cbb663f6f51
Dan Chopskie <dchopskie@gmail.com>
2024-11-30 23:56:45 -0600

    Day 1

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -1,10 +1,11 @@
 ﻿using System.Globalization;
+using System.Security.AccessControl;
 
 namespace AdventOfCode;
 
 public class Day01 : BaseDay
 {
-    private readonly List<List<int>> _input;
+    private readonly (List<int>, List<int>) _input;
 
     public Day01()
     {
@@ -14,25 +15,35 @@ public class Day01 : BaseDay
     public override ValueTask<string> Solve_1() => new($"{Part_1()}");
     public override ValueTask<string> Solve_2() => new($"{Part_2()}");
 
-    private List<List<int>> ParseInput()
+private (List<int>, List<int>) ParseInput()
+{
+    var allLines = File.ReadAllLines(InputFilePath);
+    
+    List<int> left = new List<int>();
+    List<int> right = new List<int>();
+
+    foreach (var line in allLines)
     {
-        var allLines = File.ReadAllText(InputFilePath);
-        var result = allLines
-                         .Split(new[] { "\r\n\r\n", "\n\n" }, StringSplitOptions.None)
-                         .Select(block => block.Split(['\r', '\n'], StringSplitOptions.RemoveEmptyEntries)
-                                               .Select(int.Parse)
-                                               .ToList())
-                         .ToList();
-        return result;
+        var parts = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        left.Add(int.Parse(parts[0]));
+        right.Add(int.Parse(parts[1]));
     }
 
+    return (left, right);
+}
+
     public int Part_1() {
-        return _input.Max(sublist => sublist.Sum());
+        var sortedList1 = _input.Item1.OrderBy(x => x).ToList();
+        var sortedList2 = _input.Item2.OrderBy(x => x).ToList();
+
+        return sortedList1.Zip(sortedList2, (x, y) => Math.Abs(x - y)).Sum();;
     }
 
     public int Part_2() {
-        return _input.OrderByDescending(sublist => sublist.Sum())
-               .Take(3)
-               .Sum(sublist => sublist.Sum());
+        var frequencyDict = _input.Item2
+            .GroupBy(n => n)
+            .ToDictionary(g => g.Key, g => g.Count());
+        return _input.Item1.Select(x => x * frequencyDict.GetValueOrDefault(x, 0)).Sum();
     }
 }
